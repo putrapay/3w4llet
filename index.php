7226850e942634cbf7f9322d173dde9a8526cefd
putrapay <108320444+putrapay@users.noreply.github.com>
2022-07-16 15:29:00 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="in">
+<head>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8247290737950345"
+     crossorigin="anonymous"></script>
+    <title>PUTRA CELL</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+        body {
+            background:#f1f1f1;
+            padding:0;
+            margin:0;
+            font-family:arial;
+        }
+        html, body {
+            height:100%;
+            }
+    </style>
+    
+</head>
+<body>
+    <style>
+        #list {
+        background-color:white;
+        box-shadow:0 1px 3px 0 rgba(0,0,0,0.1);
+        border-radius:4px;
+        width:100%;
+        height:auto;
+        position:relative;
+        }
+.pos li {
+        display:block;
+        padding:3%;
+    }
+    .pos ul {
+        padding:0;
+        margin:0;
+    }
+.box {
+position: relative;
+}
+.box input:focus + label,
+.box input:valid + label,
+.box textarea:focus + label,
+.box textarea:valid + label {
+color: #2196f3;
+top: -13px;
+}
+.box input:focus + label:after,
+.box input:valid + label:after,
+.box textarea:focus + label:after,
+.box textarea:valid + label:after {
+}
+.wrapper label {
+position: absolute;
+top: 19%;
+left: 2%;
+color: grey;
+text-decoration:none;
+background: white;
+width:24%;
+text-align:center;
+transition: 0.3s ease-in-out;
+border-radius: 50px;
+}
+.wrapper a {
+color: grey;
+border-radius: 50px;
+}
+#opi:hover {
+    color:#2196f3;
+}
+.boxx {
+position: relative;
+}
+.boxx input:focus + label,
+.boxx input:valid + label,
+.boxx textarea:focus + label,
+.boxx textarea:valid + label {
+color: #2196f3;
+top: -13px;
+left:36%;
+}
+.piol {
+    color:#2196f3;
+}
+.rig {
+    color:grey;
+}
+.boxx input:focus + label:after,
+.boxx input:valid + label:after,
+.boxx textarea:focus + label:after,
+.boxx textarea:valid + label:after {
+}
+.btn-success:hover {
+    border:1px solid grey;
+    color:grey;
+}
+.container h5 {
+  color: #2196f3;
+}
+</style>
+    <div style="background-color:#2196f3;width:100%;height:60px;position:relative;">
+        <br/>
+        <br/>
+        
+    <div class="container" style="background:white;height:122%;width:92%;border-radius:4px;box-shadow:0 1px 3px 0 rgba(0,0,0,0.1);position:relative;">
+        <br/>
+<?php 
+  $nomor = $_GET["nomor"]
+?>
+        <div class="">
+            <form method="get">
+<div class="wrapper">
+    <p class="box">
+        <input class="form-control" id="email" type="number" required="required" name="nomor" class="form-control" value="" placeholder="08xxxx"> 
+        <label for="email"><small>Nomor Dana</small></label>
+        <span class="fa fa-address-book form-control-feedback" style="color: #2196f3;font-size:160%;"></span>
+    </p>
+</div>
+</div>
+</form>
+<br/>
+</div>
+    </div>
+    </div>
+   <h4 style="color:transparent;">error  709</h4>
+   <h6 style="color:transparent;">error 809</h6>
+        <img src="https://1.bp.blogspot.com/-xZxBQWLU_Lo/YOfDOiHb_kI/AAAAAAAABk0/1r5FJ_Yj5GEcP2oFQ9d72ittlO-2B7zvACLcBGAsYHQ/s604/IMG_20210709_103107_789.jpg" alt="Loading" style="width:100%;">
+
+<br/>
+<br/>
+<div id="pusat">
+    <div class="">
+        <div class="pos">
+            <ul>
+
+        <!--Dana 20RB-->
+                <li>
+                <div id="list">
+                 <div class="container">
+                  <h5>Dana 20.000</h5>
+                    <a href="https://putrapayment.bukaolshop.site/produk/telkomsel-1-000-907945/?catatan=<?php echo $nomor ?>"><button style="float:right;border-color:#2196f3;color:#2196f3;background:none;" class="btn btn-success">Beli</button></a>             
+<small style="color:#2196f3;">Topup Dana</small><br>
+<small style="color:#2196f3;">Rp 20.400</small><br>
+<small style="color:transparent;">error89</small>
+<small style="color:transparent;">eror67</small>                        
+                 </div>
+                </div>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</div>
+<br/>
+</body>
+</html>
